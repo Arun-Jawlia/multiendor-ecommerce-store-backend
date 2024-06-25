f97c6e44304a542a6024e132409cfd9dddfa2670
Arun Jawlia <arunkumar08.mk@gmail.com>
2024-06-25 08:14:55 +0530

    Update Cors error

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/test", (req, res) => {
 // app.use("/", express.static(path.join(__dirname, "./uploads")))
 app.use(
   cors({
-    origin: "*",
+    origin: "https://multivendor-ecommerce-store-7d3tu3k9r-arunjawlias-projects.vercel.app",
     credentials: true,
   })
 );

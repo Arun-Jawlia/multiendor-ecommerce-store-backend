e54e03a1dfa5597bc6e347cbc38ccd3488b7bda6
Arun Jawlia <arunkumar08.mk@gmail.com>
2024-06-25 07:48:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const UserRouter = require("./controller/user.controller");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello world this is my multivendor ecommerce store");
 });
 // app.use("/", express.static(path.join(__dirname, "./uploads")))
